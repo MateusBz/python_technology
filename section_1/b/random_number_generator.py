4047ca6b3eb057eaeafe9e3fa17ab9ebc9ff6f0b
mateusz <mateusz@gmail.com>
2019-10-26 12:23:10 +0200

    section_1 solve

---
+++ section_1/b/random_number_generator.py
@@ -0,0 +1,29 @@
+"""
+    Generator liczb naturalnych.
+    Funkcje użytkowe.
+"""
+import random
+
+def generate_number_between(min, max):
+    """Losuje liczbę z przedziału <min, max>.
+
+    :param min:
+    :param max:
+    :return:
+    """
+    x = random.randint(min, max)
+    return x
+
+
+def generate_until_drawn(number, min, max):
+    """Losuje liczbę z przedzialu <min, max> tak dlugo az wylosuje liczbe number.
+
+    :param number:
+    :param min:
+    :param max:
+    :return: ilosc potrzebnych losowan do ostatecznego wylosowania liczby number.
+    """
+    index = 0
+    while generate_number_between(min, max) != number:
+        index += 1
+    return index
